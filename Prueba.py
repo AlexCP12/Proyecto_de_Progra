3ae60ae24650171114c0c98f5047caa83d2950d4
AlexCP12 <elsie2229@gmail.com>
2021-08-04 02:13:22 +0200

    Productos
    Se agregaron las listas de productos y se agrego una imagen en la pantalla principal

--- Prueba.py
+++ Prueba.py
@@ -1,14 +1,18 @@
 import os
 from tkinter import * 
 from tkinter import messagebox
+
 os.system("cls")
 
-
+def closeWindow():
+    Toplevel.destroy()
 
 def crearFactura():
     ventanacf = Toplevel()
     ventanacf.geometry("450x400+100+100")
     ventanacf.title("Crear Factura")
+    ventanacf.configure(bg="#cdcdcb")
+
 
     global factura 
     global codigo 
@@ -32,7 +36,9 @@ def crearFactura():
     txtCantidad = Entry(ventanacf,textvariable=cantidad,width=30).place(x=10,y=250)
     
     
-    btnCFactura = Button(ventanacf,text="Crear Factura",command=CFactura, font=("Agency FB",14), width=12).place(x=20,y=280)
+    btnCFactura = Button(ventanacf,text="Crear Factura",command=CFactura, font=("Agency FB",14), width=15).place(x=20,y=280)
+    btnCFactura = Button(ventanacf,text="Salir",command=ventanacf.destroy, font=("Agency FB",14), width=12).place(x=180,y=280)
+
 
 def CFactura():
     global facturas
@@ -75,8 +81,9 @@ def agregarFactura():
     lblCantidad = Label(ventanaAf,text="Indique la cantidad de productos:", font=("Agency FB", 14)).place(x=10,y=220)
     txtCantidad = Entry(ventanaAf,textvariable=cantidad,width=30).place(x=10,y=250)
         
-    btnCFactura = Button(ventanaAf,text="Agregar Producto",command=agProducto, font=("Agency FB",14), width=12).place(x=20,y=280)
-    
+    btnCFactura = Button(ventanaAf,text="Agregar Producto",command=agProducto, font=("Agency FB",14), width=15).place(x=20,y=280)
+    btnCFactura = Button(ventanaAf,text="Salir",command=ventanaAf.quit, font=("Agency FB",14), width=12).place(x=180,y=280)
+
 def agProducto():
     global facturas
     global factura 
@@ -137,14 +144,23 @@ def mensajeFac ():
 ventana = Tk()
 ventana.geometry("450x300+100+100")
 ventana.title("Sistema de Factutación")
+
+
+img = PhotoImage(file="imagen.gif")
+label = Label(ventana,image=img)
+label.place(x=0, y=0)
+
+
 facturas = {}
-factura = IntVar()
+listaProductos = {10:["Arroz",1200],15:["Frijoles",1000],30:["Papas tostadas",100],45:["Fideos", 800],300:["tortillas",750],201:["Carton de Huevos",1500],102:["Imperial",850],603:["Leche",900],507:["Pan",500],356:["Queso",1000],709:["Atun",1100],908:["Aceite",950],167:["Galletas",500]}
+factura = IntVar()  
 codigo = IntVar()
 detalle= StringVar()
 precio = IntVar()
 cantidad= IntVar()
-btnCrearFactura = Button(ventana,text="Crear Factura",command=crearFactura, font=("Agency FB",14), width=12).place(x=200,y=20)
-btnAgregar = Button(ventana,text="Agregar Factura", command=agregarFactura, font=("Agency FB",14), width=12).place(x=200,y=70)
-btnCalcular = Button(ventana,text="Calcular",command=calcularTotalVentas, font=("Agency FB",14), width=12).place(x=200,y=120)
-btnBuscar = Button(ventana,text="Buscar",command=buscarFactura, font=("Agency FB",14), width=12).place(x=200,y=170)
+btnCrearFactura = Button(ventana,text="Crear Factura",command=crearFactura, font=("Agency FB",14), bg="#59DE8A", width=14).place(x=200,y=20)
+btnAgregar = Button(ventana,text="Agregar Producto", command=agregarFactura, font=("Agency FB",14), bg="#59DE8A",width=14).place(x=200,y=70)
+btnCalcular = Button(ventana,text="Buscar Factura",command=buscarFactura, font=("Agency FB",14), bg="#59DE8A",width=14).place(x=200,y=120)
+btnBuscar = Button(ventana,text="Cierre total",command=calcularTotalVentas, font=("Agency FB",14),bg="#59DE8A", width=14).place(x=200,y=170)
+btnBuscar = Button(ventana,text="Salir",command=ventana.quit, font=("Agency FB",14),bg="coral3",width=14).place(x=200,y=220)
 ventana.mainloop()
